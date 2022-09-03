dbd37b1535023dcbffccbc1fa0b6aed3088c3d0c
coder-sadhin <codersadhin@gmail.com>
2022-09-03 17:34:37 +0600

    new cound and category added fixed some bugs

--- test.js
+++
@@ -1,7 +0,0 @@
-const hele = "hekl. hlkjk. jasjfdo. kjafkdj. ajfkjd. lkjfkj. lkajfk hekl. hlkjk. jasjfdo. kjafkdj. ajfkjd. lkjfkj. lkajfkhekl. hlkjk. jasjfdo. kjafkdj. ajfkjd. lkjfkj. lkajfkhekl. hlkjk. jasjfdo. kjafkdj. ajfkjd. lkjfkj. lkajfkhekl. hlkjk. jasjfdo. kjafkdj. ajfkjd. lkjfkj. lkajfkhekl. hlkjk. jasjfdo. kjafkdj. ajfkjd. lkjfkj. lkajfkhekl. hlkjk. jasjfdo. kjafkdj. ajfkjd. lkjfkj. lkajfkhekl. hlkjk. jasjfdo. kjafkdj. ajfkjd. lkjfkj. lkajfk";
-// const data = hele.split(".");
-// console.log(data.slice(0, 2));
-// const b = data.slice(0, 2);
-// console.log(b.join('.'));
-const d = hele.substring(0, 1000)
-console.log(d);
